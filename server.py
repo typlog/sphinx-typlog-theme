58c7a125ed0599d646b04b8512baf75e4c6ec913
Hsiaoming Yang <me@lepture.com>
2017-11-14 21:39:09 +0900

    Use a livereload server for development

---
+++ server.py
@@ -0,0 +1,8 @@
+from livereload import Server, shell
+
+server = Server()
+server.watch('assets/*', shell('npm run build'))
+server.watch('docs/*', shell('make docs'))
+server.watch('sphinx_typlog_theme/*', shell('make docs'))
+server.watch('sphinx_typlog_theme/static/*', shell('make docs'))
+server.serve(root='docs/_build/html', port=5234)
