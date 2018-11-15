9b0228b5c37db4c5c00b76b82956284a0d3b2f8b
Hsiaoming Yang <me@lepture.com>
2018-11-15 23:14:47 +0900

    Try load information from readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,7 @@ from sphinx_typlog_theme import __version__
 from sphinx_typlog_theme import (
     add_badge_roles,
     add_github_roles,
+    load_readthedocs,
 )
 
 extensions = ['sphinx.ext.autodoc']
@@ -59,8 +60,8 @@ html_theme_options = {
     'color': '#E8371A',
     'logo': 'typlog-512.png',
     'description': 'A sphinx theme designed by Typlog.',
-    'github_user': 'typlog',
-    'github_repo': 'sphinx-typlog-theme',
+    # 'github_user': 'typlog',
+    # 'github_repo': 'sphinx-typlog-theme',
     'meta_html': '<meta name="generator" content="sphinx">',
 }
 html_theme_path = [".."]
@@ -84,4 +85,5 @@ html_sidebars = {
 
 def setup(app):
     add_badge_roles(app)
+    load_readthedocs(app)
     add_github_roles(app, 'lepture/authlib')
